b31cc17ffff09c900fdd7fdc074cea54e7441948
Sukaato <sukaato.dev@gmail.com>
2019-12-23 12:22:46 +0100

    Make stencil github pages

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,15 +4,12 @@ import { sass } from '@stencil/sass';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
+  namespace: 'coincoins-insolites',
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
   outputTargets: [
-    {
-      type: 'www',
-      // comment the following line to disable service workers in production
-      serviceWorker: null,
-      baseUrl: 'https://myapp.local/'
-    }
+    { type: 'dist' },
+    { type: 'www' }
   ],
   plugins: [
     sass()
