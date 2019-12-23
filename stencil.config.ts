ad4e3cbf993ec26897e61991415c33f891605a24
Sukaato <sukaato.dev@gmail.com>
2019-12-23 12:36:24 +0100

    Make github page

--- stencil.config.ts
+++ stencil.config.ts
@@ -8,18 +8,8 @@ export const config: Config = {
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
   outputTargets: [
-<<<<<<< HEAD
     { type: 'dist' },
     { type: 'www' }
-=======
-    {
-      type: 'www',
-      empty: false
-      // comment the following line to disable service workers in production
-      // serviceWorker: null,
-      // baseUrl: 'https://myapp.local/'
-    }
->>>>>>> master
   ],
   plugins: [
     sass()
